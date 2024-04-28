3495791b205ee9495073690b8f5251ce5a40a5b7
eporada <e.porada8@gmail.com>
2024-04-28 17:59:04 +0200

    editing and adding med

--- Models/DbHelperMethods.cs
+++ Models/DbHelperMethods.cs
@@ -33,6 +33,26 @@ namespace kokos.Models
             db.SaveChanges();
         }
 
+        public Medication GetMed(int medId)
+        {
+            return db.Meds.FirstOrDefault(m => m.Id == medId);
+        }
+
+        public void DeleteMed(string medName)
+        {
+            var med = db.Meds.FirstOrDefault(m => m.Name == medName);
+            if (med != null)
+            {
+                db.Meds.Remove(med);
+                db.SaveChanges();
+            }
+        }
+
+        public int GetMedId(string medName)
+        {
+            return db.Meds.FirstOrDefault(m => m.Name == medName).Id;
+        }
+
         public bool DoesMedExist(string medName)
         {
             return db.Meds.Any(m => m.Name == medName);
@@ -51,15 +71,21 @@ namespace kokos.Models
         }
 
 
-        public Medication GetMed(int medId)
+        public PatientsMeds GetPatientMed(int id)
         {
-            return db.Meds.FirstOrDefault(m => m.Id == medId);
+            return db.PatientsMeds.FirstOrDefault(m => m.Id == id);
+
         }
 
-        public PatientsMeds GetPatientMed(string userId, int medId)
+        public void DeletePatientMed(int id)
         {
-            return db.PatientsMeds.FirstOrDefault(m => m.MedId == medId && m.UserId == userId);
+            var pMed = db.PatientsMeds.FirstOrDefault(m => m.Id == id);
 
+            if (pMed != null)
+            {
+                db.PatientsMeds.Remove(pMed);
+                db.SaveChanges();
+            }
         }
         
     }
